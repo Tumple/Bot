fe4a9b82d2c4f455b13ba6e885051dba129cabc5
Tumple <vlad.buldakov.1994@bk.ru>
2025-01-19 13:36:15 +0300

    NewMethodForTextMessage

--- openAI/obj/Debug/net8.0/openAI.AssemblyInfo.cs
+++ openAI/obj/Debug/net8.0/openAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("openAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e2e0a48150411eca4fba0345d887cb40f56574")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e7e595fb433642a4c6c02918f32ef9e8fbe0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("openAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("openAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
